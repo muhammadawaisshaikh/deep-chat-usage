d41719c9fb0ef19037b5c3eeeed6cdf43e170c17
Muhammad Awais <muhammad.awais@royalcyber.com>
2024-06-13 11:20:53 +0500

    used deep-chat in react vite app

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
 import { useState } from 'react'
 import './App.css'
+import { DeepChat } from 'deep-chat-react'
 
 function App() {
   return (
-    <>
-      <h1>Ai Powered ChatBot</h1>
-      <p>using deep-chat</p>
-    </>
+    <div className='container pt-5'>
+      <div className='text-center'>
+        <h1>Ai Powered ChatBot</h1>
+        <p>using deep-chat</p>
+
+        <div className="row justify-content-center mt-5">
+          <div className="col-6">
+            <DeepChat />
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
